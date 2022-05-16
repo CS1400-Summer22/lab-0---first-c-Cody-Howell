8a8816a2e45bf33241f7b6700f7bd204d3408261
Cody Howell <codyhowell44@gmail.com>
2022-05-16 11:43:59 -0600

    finished lab 1

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+//Console.WriteLine("Hello, World!");
+
+
+/*string friend = "Brayden";
+Console.WriteLine($"{friend} is my friend.");*/
+
+
+/*string firstFriend = "Maria";
+string secondFriend = "Sage";
+Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");*/
+
+
+/*string greeting = "      Hello World!       ";
+Console.WriteLine($"[{greeting}]");
+
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.TrimEnd();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.Trim();
+Console.WriteLine($"[{trimmedGreeting}]");*/
+
+
+/*string sayHello = "Hello World!";
+Console.WriteLine(sayHello);
+sayHello = sayHello.Replace("Hello", "Greetings");
+Console.WriteLine(sayHello);
+
+Console.WriteLine(sayHello.ToUpper());
+Console.WriteLine(sayHello.ToLower());
+Console.WriteLine(sayHello.ToTitleCase());*/
+
+
+/*string songLyrics = "You say goodbye, and I say hello";
+Console.WriteLine(songLyrics.Contains("goodbye"));
+Console.WriteLine(songLyrics.Contains("greetings"));
+
+if (songLyrics.Contains("goodbye") == true){
+    Console.WriteLine(songLyrics);
+}*/
+
+
+//Challenge
+
+string songLyrics = "You say goodbye, and I say hello";
+Console.WriteLine(songLyrics.StartsWith("You"));
+Console.WriteLine(songLyrics.StartsWith("goodbye"));
+Console.WriteLine(songLyrics.EndsWith("hello"));
+Console.WriteLine(songLyrics.EndsWith("goodbye"));
