3d593ceee27256fdae717d8948b58429c0637746
Moosenwolf <codyhowell44@gmail.com>
2022-05-17 09:59:12 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,19 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Clear();
-//Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
 
 
-/*string friend = "Brayden";
-Console.WriteLine($"{friend} is my friend.");*/
+string friend = "Brayden";
+Console.WriteLine($"{friend} is my friend.");
 
 
-/*string firstFriend = "Maria";
+string firstFriend = "Maria";
 string secondFriend = "Sage";
 Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
-Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");*/
+Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
 
 
-/*string greeting = "      Hello World!       ";
+string greeting = "      Hello World!       ";
 Console.WriteLine($"[{greeting}]");
 
 string trimmedGreeting = greeting.TrimStart();
@@ -23,26 +23,26 @@ trimmedGreeting = greeting.TrimEnd();
 Console.WriteLine($"[{trimmedGreeting}]");
 
 trimmedGreeting = greeting.Trim();
-Console.WriteLine($"[{trimmedGreeting}]");*/
+Console.WriteLine($"[{trimmedGreeting}]");
 
 
-/*string sayHello = "Hello World!";
+string sayHello = "Hello World!";
 Console.WriteLine(sayHello);
 sayHello = sayHello.Replace("Hello", "Greetings");
 Console.WriteLine(sayHello);
 
 Console.WriteLine(sayHello.ToUpper());
 Console.WriteLine(sayHello.ToLower());
-Console.WriteLine(sayHello.ToTitleCase());*/
+//Console.WriteLine(sayHello.ToTitleCase());
 
 
-/*string songLyrics = "You say goodbye, and I say hello";
+string songLyrics = "You say goodbye, and I say hello";
 Console.WriteLine(songLyrics.Contains("goodbye"));
-Console.WriteLine(songLyrics.Contains("greetings"));
+Console.WriteLine(songLyrics.Contains("greetings");
 
 if (songLyrics.Contains("goodbye") == true){
     Console.WriteLine(songLyrics);
-}*/
+}
 
 
 //Challenge
